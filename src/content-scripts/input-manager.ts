f206c93ed9ff541f4c78118c3c83b1f53bfce4b9
shneky <shneky@protonmail.com>
2019-02-24 17:07:04 +0100

    made it more object oriented

--- src/content-scripts/input-manager.ts
+++ src/content-scripts/input-manager.ts
@@ -1,27 +1,66 @@
+enum Modifier {
+  Tmux,
+  Ctrl,
+  None
+}
+
+enum TmuxEvent {
+  Activate = 'activate',
+  Next = 'next',
+  Previous = 'previous',
+  Create = 'create',
+  None = 'none',
+}
+
+class KeyEvent {
+  constructor(public key: string,
+    public modifier?: Modifier,
+    public tmuxEvent?: TmuxEvent,
+    public preventDefault?: boolean) { }
+
+  private kvToModifier(kv: KeyboardEvent, tmuxActivated: boolean): Modifier {
+    if (tmuxActivated) {
+      return Modifier.Tmux;
+    } else if (kv.ctrlKey) {
+      return Modifier.Ctrl;
+    }
+    return Modifier.None;
+  }
+
+  public equals(kv: KeyboardEvent, tmuxActivated: boolean): boolean {
+    return (this.kvToModifier(kv, tmuxActivated) == this.modifier) && this.key == kv.key;
+  }
+}
+
 class KeyboardEventHandler {
-  keyToEventMap: Map<string, string> = new Map(
-    [
-      ['n', 'next'],
-      ['p', 'previous'],
-      ['c', 'create'],
-      ['b', 'back']
-    ]
-  );
-  keysToDisable: string[] = ['n', 'p', 'b'];
-  tmuxKey = 'b';
-  tmuxKeyActivated = false;
+  keyEvents: KeyEvent[] = [
+    new KeyEvent('n', Modifier.Tmux, TmuxEvent.Next, true),
+    new KeyEvent('p', Modifier.Tmux, TmuxEvent.Previous, true),
+    new KeyEvent('c', Modifier.Tmux, TmuxEvent.Create, true),
+    new KeyEvent('b', Modifier.Ctrl, TmuxEvent.Activate, true),
+    new KeyEvent('n', Modifier.Ctrl, TmuxEvent.None, true),
+    new KeyEvent('p', Modifier.Ctrl, TmuxEvent.None, true),
+    new KeyEvent('b', Modifier.None, TmuxEvent.None, true),
+  ]
+  tmuxActivated = false;
 
   listen() {
-    document.addEventListener('keydown', (kv) => this.handleKeyDown(kv));
+    window.addEventListener('keydown', (kv) => this.handleKeyDown(kv));
   }
 
   handleKeyDown(kv: KeyboardEvent): void {
-    if (kv.ctrlKey && this.keysToDisable.includes(kv.key)) { kv.preventDefault() };
-    if (kv.ctrlKey && kv.key == this.tmuxKey) { this.tmuxKeyActivated = true };
-
-    if (this.tmuxKeyActivated && kv.key != this.tmuxKey) {
-      this.tmuxKeyActivated = false;
-      browser.runtime.sendMessage(this.keyToEventMap.get(kv.key));
+    for (const k of this.keyEvents) {
+      if (k.equals(kv, this.tmuxActivated)) {
+        if (k.preventDefault) {
+          kv.preventDefault();
+        }
+        if (k.tmuxEvent == TmuxEvent.Activate) {
+          this.tmuxActivated = true;
+        } else if (this.tmuxActivated) {
+          this.tmuxActivated = false;
+          browser.runtime.sendMessage(k.tmuxEvent.toString());
+        }
+      }
     }
   }
 }
